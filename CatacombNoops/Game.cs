8105790c34114c09b80f92da76b9323e4ac566c9
CatacombNoop <oleg.sidorov.o.o@gmail.com>
2019-06-22 02:29:16 +0600

    Headache

---
+++ CatacombNoops/Game.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace CatacombNoops
+{
+    public class Game :  Microsoft.Xna.Framework.Game {
+        
+        
+        private GraphicsDeviceManager _gfxDevice;
+        private SpriteBatch           _spriteBatch;
+    }
+}
--- CatacombNoops/Program.cs
+++ CatacombNoops/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace CatacombNoops
+﻿using Microsoft.Xna.Framework;
+
+namespace CatacombNoops
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            using (var g = new Game ()) {
+                g.Run ();
+            }   
         }
     }
 }
